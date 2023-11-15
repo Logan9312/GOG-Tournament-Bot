0ab549b78eac3b6f131b89e758da93add186c4ce
Logan <lgntravis@gmail.com>
2023-11-14 23:49:29 -0700

    Fixing error

--- commands/base.go
+++ commands/base.go
@@ -9,7 +9,6 @@ import (
 var StartCommand = discordgo.ApplicationCommand{
 	Name:        "start",
 	Description: "Starts a tournament",
-	Options:     []*discordgo.ApplicationCommandOption{},
 }
 
 // TODO Track tournament number
@@ -22,24 +21,9 @@ func StartTournament(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			{
 				Title:       fmt.Sprintf("Tournament %d!", tournamentNumber),
 				Description: "Tournament Information:",
-				Timestamp:   "",
 				Color:       0x00bfff,
-				Footer:      &discordgo.MessageEmbedFooter{},
-				Image:       &discordgo.MessageEmbedImage{},
-				Thumbnail:   &discordgo.MessageEmbedThumbnail{},
-				Video:       &discordgo.MessageEmbedVideo{},
-				Provider:    &discordgo.MessageEmbedProvider{},
-				Author:      &discordgo.MessageEmbedAuthor{},
-				Fields:      []*discordgo.MessageEmbedField{},
 			},
 		},
-		TTS:             false,
-		Components:      []discordgo.MessageComponent{},
-		Files:           []*discordgo.File{},
-		AllowedMentions: &discordgo.MessageAllowedMentions{},
-		Reference:       &discordgo.MessageReference{},
-		StickerIDs:      []string{},
-		Flags:           0,
 	})
 	if err != nil {
 		fmt.Println("Error sending message:", err)
@@ -51,14 +35,6 @@ func StartTournament(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		Data: &discordgo.InteractionResponseData{
 			TTS:             false,
 			Content:         "Success",
-			Components:      []discordgo.MessageComponent{},
-			Embeds:          []*discordgo.MessageEmbed{},
-			AllowedMentions: &discordgo.MessageAllowedMentions{},
-			Files:           []*discordgo.File{},
-			Flags:           0,
-			Choices:         []*discordgo.ApplicationCommandOptionChoice{},
-			CustomID:        "",
-			Title:           "",
 		},
 	})
 	if err != nil {
